172104c4b4ee258408854ea73c9ef2d4d85b67c9
mcolic <mcolic@na.edu>
2015-09-14 14:48:41 -0500

    Add swap.py

---
+++ swap.py
@@ -0,0 +1,17 @@
+def swap1(x,y) :
+    x=y
+    y=x
+    return (x,y)
+
+def swap2(x,y) :
+    return (y,x)
+
+def swap3(x,y) :
+    z=x
+    x=y
+    y=z
+    return (x,y)
+
+def swap4(x,y) :
+    x,y=y,x
+    return (x,y)
